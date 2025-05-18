42473bf5a2f64d23cd6c0b988bc790f285ed76c9
sivaprasadtalapagala <sivaprasadreddytalapagala@gmail.com>
2025-05-18 22:03:51 +0530

    @input decorator

--- src/app/decorators/child/child.component.ts
+++ src/app/decorators/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child.component.scss']
 })
 export class ChildComponent implements OnInit {
+  @Input() childVariable:any;
 
   constructor() { }
 
--- src/app/decorators/parent/parent.component.ts
+++ src/app/decorators/parent/parent.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent.component.scss']
 })
 export class ParentComponent implements OnInit {
+  sendToParent ="I am from parent component loaded through child"
 
   constructor() { }
 
